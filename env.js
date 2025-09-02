831bfe71a7c8aa404b7adfe7f361f7ab964ffb1f
夏诗涵 <georgeeasop@gmail.com>
2025-09-02 14:50:22 +0800

    Update and rename env.example.js to env.js

--- env.js
+++ env.js
@@ -1,10 +1,10 @@
 // API地址
-window.routerBase = 'http://127.0.0.1:8000/'
+window.routerBase = 'https://vowa.top/'
 window.settings = {
   // 站点名称
-  title: 'Xboard',
+  title: 'Vowa',
   // 站点描述
-  description: 'Xboard',
+  description: 'Vowa',
   assets_path: '/assets',
   // 主题色
   theme: {
